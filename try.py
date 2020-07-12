684b24ff73d31a2689b4527b0968b09165da79e9
Ali Talha Atici <alitatici@gmail.com>
2020-07-12 18:56:08 +0300

    aaa

--- try.py
+++ try.py
@@ -20,16 +20,25 @@ class Ui_MainWindow(object):
         self.spinBox.setGeometry(QtCore.QRect(10, 20, 61, 22))
         self.spinBox.setMinimum(1)
         self.spinBox.setMaximum(4)
+        self.spinBox.setSingleStep(1)
+        self.spinBox.setProperty("value", 1)
         self.spinBox.setObjectName("spinBox")
-        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(80, 20, 701, 22))
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.lineEdit = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
-        self.lineEdit.setObjectName("lineEdit")
-        self.horizontalLayout.addWidget(self.lineEdit)
+        value = self.spinBox.value()
+
+        if value == 1 :
+            self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+            self.horizontalLayoutWidget.setGeometry(QtCore.QRect(80, 20, 701, 22))
+            self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+            self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+            self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+            self.horizontalLayout.setObjectName("horizontalLayout")
+            self.lineEdit_2 = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
+            self.lineEdit_2.setObjectName("lineEdit_2")
+            self.horizontalLayout.addWidget(self.lineEdit_2)
+            self.lineEdit = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
+            self.lineEdit.setObjectName("lineEdit")
+            self.horizontalLayout.addWidget(self.lineEdit)
+        
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
@@ -42,16 +51,26 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        if self.spinBox.value == 2:
-            self.lineEdit.setObjectName("lineEdit2")
-            self.horizontalLayout.addWidget(self.lineEdit)
-        
-        self.spinBox = 2
+        print(str(self.spinBox.value()))
+
+        value = self.spinBox.value()
+
+        if value == 2 :
+            self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+            self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(80, 50, 701, 22))
+            self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
+            self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_2)
+            self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+            self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+            self.lineEdit_3 = QtWidgets.QLineEdit(self.horizontalLayoutWidget_2)
+            self.lineEdit_3.setObjectName("lineEdit_3")
+            self.horizontalLayout_2.addWidget(self.lineEdit_3)
+
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-
+        
 
 if __name__ == "__main__":
     import sys
@@ -61,3 +80,4 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
+
